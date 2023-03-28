68f2229b4c8bc22e2ce168f4044b91d51d45ede1
Alikhadempour <ali.khpourarani823@gmail.com>
2023-03-28 17:54:38 +0330

    change Program and fix bug

--- loknat.py
+++ loknat.py
@@ -1,9 +1,7 @@
 word = input()
 cnt = 1
 for letter in word:
-    if letter == 'R':
-        cnt *= 3
-    elif letter == "L" or letter == "F" or letter == "G" or letter == "D" or letter == "T":
+    if letter == "L" or letter == "F"  or letter == "D" or letter == "T":
         cnt *= 2
 
 print(cnt)

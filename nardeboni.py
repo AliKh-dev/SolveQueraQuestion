b61b6fa2cec75c77972f0eeceaa604d1fdc553a2
Alikhadempour <ali.khpourarani823@gmail.com>
2023-03-28 19:45:39 +0330

    final project

--- nardeboni.py
+++ nardeboni.py
@@ -1,5 +1,13 @@
-number1 = int(input())
-number2 = int(input())
+number1 = abs(int(input()))
+number2 = abs(int(input()))
+if number1 == 0:
+    print(number2)
+    quit()
+elif number2 == 0:
+    print(number1)
+    quit()
+if number2 > number1:
+    number1, number2 = number2, number1
 
 while True:
     if number1 % number2 == 0:

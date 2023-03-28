02991d724fa448f21b1bf969960b150dbe1abd58
Alikhadempour <ali.khpourarani823@gmail.com>
2023-03-28 17:35:29 +0330

    First Program firs Try

--- loknat.py
+++ loknat.py
@@ -1,1 +1,9 @@
-string = input()
+word = input()
+cnt = 1
+for letter in word:
+    if letter == 'R':
+        cnt *= 3
+    elif letter == "L" or letter == "F" or letter == "G" or letter == "D" or letter == "T":
+        cnt *= 2
+
+print(cnt)

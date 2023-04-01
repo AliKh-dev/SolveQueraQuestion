28253de80060a399b7074a7436aa10921139f0a0
Alikhadempour <ali.khpourarani823@gmail.com>
2023-04-01 13:30:25 +0330

    Solve question

--- چیدمان.py
+++ چیدمان.py
@@ -4,8 +4,8 @@ sum_number = 0
 for i in range(n):
     numbers[i] = int(input())
     sum_number += numbers[i]
-average = sum_number // n 
+average = sum_number // n
 result = 0
 for number in numbers:
     result += (abs(number - average))
-print(result)
+print(result // 2)

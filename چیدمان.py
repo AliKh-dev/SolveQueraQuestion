7601ac459616cfd252f1e8911312d4306e4ed5b5
Alikhadempour <ali.khpourarani823@gmail.com>
2023-04-01 13:28:28 +0330

    First try

--- چیدمان.py
+++ چیدمان.py
@@ -1,1 +1,11 @@
 n = int(input())
+numbers = [0] * n
+sum_number = 0
+for i in range(n):
+    numbers[i] = int(input())
+    sum_number += numbers[i]
+average = sum_number // n 
+result = 0
+for number in numbers:
+    result += (abs(number - average))
+print(result)

423b42564efd59a8b0d974dee920a9447a17e288
Alikhadempour <ali.khpourarani823@gmail.com>
2023-04-01 13:19:25 +0330

    Solve ramz question

--- رمز.py
+++ رمز.py
@@ -4,8 +4,11 @@ numbers = [0] * n
 cnt = 0
 for i in range(n):
     numbers[i] = input()
-    if numbers[i].index(code[(n-1) - i]) > (len(numbers[i]) // 2):
-        cnt += len(numbers[i]) - numbers[i].index(code[(n-1) - i])
-    else:    
-        cnt += numbers[i].index(code[(n-1) - i])
+    if numbers[i].find(code[i]) >= 5:
+        # print(numbers[i].find(code[i]))
+        cnt += (9 - numbers[i].find(code[i]))
+    else:
+        # print(numbers[i].find(code[i]))
+        cnt += numbers[i].find(code[i])
+    # print("-----------")
 print(cnt)

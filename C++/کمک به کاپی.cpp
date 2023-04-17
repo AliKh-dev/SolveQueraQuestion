17d3312a23987f22019e861b9c3be0d4c5ace978
Alikhadempour <ali.khpourarani823@gmail.com>
2023-04-17 21:42:26 +0330

    Solve komak be copy

---
+++ C++/کمک به کاپی.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    int n;
+    cin >> n >> str;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "copy of ";
+    }
+    cout << str;
+}
